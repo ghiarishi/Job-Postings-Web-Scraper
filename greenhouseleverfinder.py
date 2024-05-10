1c2f9afaf590364c0721c054cccfc651fd2adac9
ghiarishi <ghiarishi@gmail.com>
2024-05-10 18:42:18 -0400

    URL input -> job links working

--- greenhouseleverfinder.py
+++ greenhouseleverfinder.py
@@ -1,12 +1,45 @@
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urlsplit, urlunsplit
-
+from urllib.parse import urlsplit, parse_qs, urlunsplit
+from googlesearch import search
+ 
 def clean_url(url):
-    # Split the URL into components
-    split_url = urlsplit(url)
-    # Reconstruct the URL without the query and fragment components
-    cleaned_url = urlunsplit((split_url.scheme, split_url.netloc, split_url.path, '', ''))
+    # Parse the query parameters
+    query = urlsplit(url).query
+    parsed_query = parse_qs(query)
+    
+    # Extract the actual job link from the 'q' parameter
+    if 'q' in parsed_query:
+        job_url = parsed_query['q'][0]
+    else:
+        return url  # Return the original URL if 'q' is not present
+
+    if "lever.co" in job_url:
+        # Split the path after the netloc to preserve the base URL
+        path_parts = job_url.split('/')
+        if len(path_parts) > 4:
+            cleaned_path = '/'.join(path_parts[:5])  # Limit to the first 5 parts
+        else:
+            cleaned_path = '/'.join(path_parts)
+        
+        # Construct the cleaned URL
+        netloc = urlsplit(job_url).netloc
+        cleaned_url = f"{cleaned_path}"
+    
+    elif "greenhouse.io" in job_url:
+        # Cut off at the first non-numeric character after "jobs/"
+        path_parts = job_url.split("/")
+        if len(path_parts) > 5:
+            # Ensure the 6th item is numeric
+            numeric_sixth_item = ''.join([char for char in path_parts[5] if char.isnumeric()])
+            # Concatenate the first 6 items
+            cleaned_path = '/'.join(path_parts[:5] + [numeric_sixth_item])
+            cleaned_url = f"{cleaned_path}"
+        else:
+            cleaned_url = job_url
+    else:
+        cleaned_url = job_url
+    
     return cleaned_url
 
 def get_job_details(url):
@@ -48,9 +81,9 @@ def get_job_details(url):
     else:
         print(f"Failed to retrieve the job page for URL: {url}")
         return None
-    
+
 # URL of the page you want to scrape
-url = 'https://www.google.com/search?q=(engineer+OR+software)+(machine+learning+OR+%22software+engineer%22+OR+%22software+develop%22+OR+%22natural+language+processing%22+OR+%22computer+vision%22+OR+%22perception%22)+site:lever.co+OR+site:greenhouse.io+location:US+-staff+-senior+-principal+-manager+-lead&sca_esv=a0ffaebd0ed0b666&source=lnt&tbs=qdr:w&sa=X&ved=2ahUKEwiPjPaImf2FAxW1LFkFHd4ODgwQpwV6BAgCEAk&biw=1862&bih=894&dpr=1.38#ip=1'
+url = 'https://www.google.com/search?q=(engineer+OR+software)+(machine+learning+OR+%22software+engineer%22+OR+%22software+develop%22+OR+%22natural+language+processing%22+OR+%22computer+vision%22+OR+%22perception%22)+site%3Alever.co+OR+site%3Agreenhouse.io+location%3AUS+-staff+-senior+-principal+-manager+-lead&oq=(engineer+OR+software)+(machine+learning+OR+%22software+engineer%22+OR+%22software+develop%22+OR+%22natural+language+processing%22+OR+%22computer+vision%22+OR+%22perception%22)+site%3Alever.co+OR+site%3Agreenhouse.io+location%3AUS+-staff+-senior+-principal+-manager+-lead&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIGCAEQRRg7MgYIAhBFGDvSAQgzODIzajBqN6gCALACAA&sourceid=chrome&ie=UTF-8#ip=1'
 
 # Send a GET request
 response = requests.get(url)
@@ -68,18 +101,29 @@ if response.status_code == 200:
     for link in job_links:
         href = link['href']
         if "https://boards.greenhouse.io" in href or "https://jobs.lever.co" in href:
-            print("Job Link: \n", href[7:])
+            cleaned_href = clean_url(href)
+            print(cleaned_href)
 else:
     print("Failed to retrieve the page")
 
-job_urls = [
-    'https://boards.greenhouse.io/waymo/jobs/5896019&sa=U&ved=2ahUKEwjGl7nsnv2FAxV8jIkEHd5vB-oQFnoECAYQAg&usg=AOvVaw0PVRfJLO3tM7qVA4PkmJoZ',
-    'https://jobs.lever.co/whoop/faeef488-7276-47f8-81d9-7087c252345c/apply&sa=U&ved=2ahUKEwjGl7nsnv2FAxV8jIkEHd5vB-oQFnoECAQQAg&usg=AOvVaw0RSY9XWQcVQaYgTg5l8753',
-    # More URLs can be added here
-]
+# job_urls = [
+#     'https://boards.greenhouse.io/waymo/jobs/5853040 '
+#     'https://boards.greenhouse.io/waymo/jobs/5853062'
+#     'https://boards.greenhouse.io/waymo/jobs/5896019'
+#     'https://boards.greenhouse.io/grammarly/jobs/5932771'
+#     'https://boards.greenhouse.io/vannevarlabs/jobs/4366534007'
+#     'https://boards.greenhouse.io/offerfit/jobs/4416058005'
+#     'https://boards.greenhouse.io/pinterestjobadvertisements/jobs/5951216'
+#     'https://boards.greenhouse.io/d2l/jobs/5947933'
+#     'https://jobs.lever.co/whoop/faeef488-7276-47f8-81d9-7087c252345c/apply'
+#     'https://boards.greenhouse.io/technergetics/jobs/4361465006'
+# ]
 
-# Fetch and print job details for each URL
-for job_url in job_urls:
-    job_info = get_job_details(job_url)
-    if job_info:
-        print(job_info)
+# # Fetch and print job details for each URL
+# for job_url in job_urls:
+#     job_info = get_job_details(job_url)
+#     if job_info:
+#         print("Job Info : ", job_info)
+#     else: 
+#         print("Info not available")
+    
